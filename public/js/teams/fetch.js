07235d60d6b5fe8aef6be0bc0c038d170b13168d
RubenDantas <rubenluisdantas@gmail.com>
2024-12-14 12:05:28 +0000

    ✨Feat
    - ficheiro .gitignore criado para ignorar os node modules quando é feito commit
    
    - adicionado comando "start" em package.json para iniciar o servidor
    
    - adicionado script "firststart" para executar os ficheiros createDatabase.js, createTables.js e iniciar o servidor. (ainda não funciona)
    
    - Adicionados modals de criar equipa e apagar todos os registos da tabela
    
    - Adicionadas funções de fetch para criar equipa, apagar uma equipa e apagar todas as equipas

--- public/js/teams/fetch.js
+++ public/js/teams/fetch.js
@@ -4,17 +4,17 @@ function getId(id){
     teamId = id;
 }
 
-//PUT
+//? PUT - EDITAR EQUIPA
 function editTeam(){
-    let name = document.getElementById(`inputTeamName`).value;
-    let initials = document.getElementById(`inputTeamInitials`).value;
-    let formedYear = document.getElementById(`inputTeamFormedYear`).value;
-    let stadium = document.getElementById(`inputTeamStadium`).value;
-    let country = document.getElementById(`inputTeamCountry`).value;
+    let name = document.getElementById(`inputTeamNameEdit`).value;
+    let initials = document.getElementById(`inputTeamInitialsEdit`).value;
+    let formedYear = document.getElementById(`inputTeamFormedYearEdit`).value;
+    let stadium = document.getElementById(`inputTeamStadiumEdit`).value;
+    let country = document.getElementById(`inputTeamCountryEdit`).value;
 
     console.log(name);
     console.log(teamId);
-    fetch(`/api/teams/${teamId}`, {
+    fetch(`/teams/${teamId}`, {
         method: "PUT",
         headers:  {
             "Content-Type": "application/json"
@@ -29,6 +29,67 @@ function editTeam(){
     })
     .then((response) => console.log(response))
     .catch((err) => console.log("Error:", err));
+
+    //refresh page
+    location.reload();
+}
+
+//* POST - CRIAR EQUIPA
+function createTeam(){
+    let name = document.getElementById(`inputTeamNameCreate`).value;
+    let initials = document.getElementById(`inputTeamInitialsCreate`).value;
+    let formedYear = document.getElementById(`inputTeamFormedYearCreate`).value;
+    let stadium = document.getElementById(`inputTeamStadiumCreate`).value;
+    let country = document.getElementById(`inputTeamCountryCreate`).value;
+
+    console.log(name);
+    fetch(`/teams/`, {
+        method: "POST",
+        headers:  {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+                "name": name!="" ? name : null,
+                "initials": initials!="" ? initials : null,
+                "formedYear": formedYear!="" ? formedYear : null,
+                "stadium": stadium!="" ? stadium : null,
+                "country": country!="" ? country : null
+        })
+    })
+    .then((response) => console.log(response))
+    .catch((err) => console.log("Error:", err));
+
+    //refresh page
+    location.reload();
+}
+
+//! DELETE - Apagar Equipa
+function deleteTeam(){
+    console.log(teamId);
+    fetch(`/teams/${teamId}`, {
+        method: "Delete",
+        headers:  {
+            "Content-Type": "application/json"
+        }
+    })
+    .then((response) => console.log(response))
+    .catch((err) => console.error("Error:", err));
+
+    //refresh page
+    location.reload();
+}
+
+//! DELETE - Apagar TODAS as equipas
+function deleteAllTeams(){
+    console.log(teamId);
+    fetch(`/teams/`, {
+        method: "Delete",
+        headers:  {
+            "Content-Type": "application/json"
+        }
+    })
+    .then((response) => console.log(response))
+    .catch((err) => console.error("Error:", err));
 
     //refresh page
     location.reload();
--- src/components/teams/routes/teamsRouter.js
+++ src/components/teams/routes/teamsRouter.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const teamsController = require("../controllers/teamsController.js");
 
 router.get("/:name", teamsController.getTeamByName); //TODO fetch
-router.get("/", teamsController.getAllTeams); //TODO fetch
+router.get("/", teamsController.getAllTeams);
 
-router.post("/", teamsController.createTeam); //TODO fetch + verifications
+router.post("/", teamsController.createTeam); //TODO verifications
 
-router.put("/:id", teamsController.editTeam); //TODO fetch + verifications
+router.put("/:id", teamsController.editTeam); //TODO verifications
 
-router.delete("/:id", teamsController.deleteTeam); //TODO fetch
+router.delete("/:id", teamsController.deleteTeam); //TODO
 router.delete("/", teamsController.deleteAllTeams); //TODO fetch
 
 module.exports = router;
